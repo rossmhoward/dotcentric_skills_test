bdeb073de520bb38c0cbe0bde279a849803180d6
Ross Howard <ross_howard@dennis.co.uk>
2021-07-01 09:34:33 +0100

    DC: Initial setup and api returning array using fetch and promises. Using vanilla JS as I'm more familiar with it and it's quicker than setting up a framework.

---
+++ script.js
@@ -0,0 +1,13 @@
+
+(function() {
+  fetch('https://api.mocki.io/v2/561baaaa').then(function (response) {
+    // The API call was successful!
+    return response.json();
+  }).then(function (data) {
+    // This is the JSON from our response
+    console.log(data);
+  }).catch(function (err) {
+    // There was an error
+    console.warn('Something went wrong.', err);
+  });
+})();
