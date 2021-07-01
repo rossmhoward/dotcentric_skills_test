b024994c64b4675776797f810f603f17601cb6bd
Ross Howard <ross_howard@dennis.co.uk>
2021-07-01 15:26:59 +0100

    DC: Last commit I should have explained the method behind the menu. At first I was going to make the mobile menu slide out over the content but I noticed the design illustrates that it should push the site to one side. So to do that I had to wrap the main content in a div, and have an off page div containing the nav items. When the menu is opened, the page content slides left, and when the nav content is closed it slides back
    Currently building out an array from the API and matching it to div elements.

--- script.js
+++ script.js
@@ -6,10 +6,23 @@
   }).then(function (data) {
     // This is the JSON from our response
     console.log(data);
+    buildContent(data);
   }).catch(function (err) {
     // There was an error
     console.warn('Something went wrong.', err);
   });
+
+  buildContent = function(data) {
+    console.dir(data.services);
+    data.services.forEach(element => {
+      console.log(element);
+    });
+
+    let servicesElements = document.querySelectorAll('.services li');
+    servicesElements.forEach(element => {
+      console.log('Services Wrappers: ', element);
+    });
+  }
 
   // Mobile Menu
   // Using a click event on the close button and on the burger menu we can toggle a class on both the main div and the nav container div to transition in and out. 
