7d811d16615660392dd0125facdf89b47a814ebc
Ross Howard <ross_howard@dennis.co.uk>
2021-07-01 15:21:15 +0100

    DC: Burger menu complete.

--- script.js
+++ script.js
@@ -10,4 +10,27 @@
     // There was an error
     console.warn('Something went wrong.', err);
   });
+
+  // Mobile Menu
+  // Using a click event on the close button and on the burger menu we can toggle a class on both the main div and the nav container div to transition in and out. 
+  const burgerMenu = document.querySelector('.delicious-burger');
+  const leftSide = document.querySelector('.mobile-left');
+  const rightSide = document.querySelector('.mobile-right');
+  const closeBtn = document.querySelector('.mobile-right img');
+  
+  console.log(document.querySelector('.mobile-right'));
+  console.log('All', burgerMenu, leftSide, rightSide, closeBtn);
+
+  burgerMenu.addEventListener( 'click', function() {
+    console.log('Transition In');
+    leftSide.classList.add('transitioned');
+    rightSide.classList.add('transitioned');
+  });
+
+  closeBtn.addEventListener( 'click', function() {
+    console.log('Transition Out');
+    leftSide.classList.remove('transitioned');
+    rightSide.classList.remove('transitioned');
+  });
+
 })();
